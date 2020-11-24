61d5a198916178b2882667331c6780269e981764
skcho22 <skcho22@student.monash.edu>
2020-11-25 07:23:43 +1100

    Update SideBar.js

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 
 const SideBar = () => {
   const [state, setState] = useState([
-    { id: 1, image: "/images/fawad1.jpg", name: "Events" },
-    { id: 2, image: "/images/fawad2.jpg", name: "Programs" },
+    { id: 1, image: "/images/fawad1.png", name: "Events" },
+    { id: 2, image: "/images/fawad2.png", name: "Programs" },
     { id: 3, image: "/images/fawad3.jpg", name: "Rick and Morty Club" },
     { id: 4, image: "/images/fawad4.jpg", name: "Membership" },
     { id: 5, image: "/images/imran1.jpg", name: "Something" },
