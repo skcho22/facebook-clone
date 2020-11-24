fcc44f3a251c8d5c860970f2aaf5f2565732335b
skcho22 <skcho22@student.monash.edu>
2020-11-25 06:40:43 +1100

    Update ChatBar.js

--- src/components/ChatBar.js
+++ src/components/ChatBar.js
@@ -2,17 +2,17 @@ import React from "react";
 
 const ChatBar = (props) => {
   const [state, setState] = React.useState([
-    { id: 1, image: "/images/fawad1.jpg", name: "Fawad Khan" },
-    { id: 2, image: "/images/fawad2.jpg", name: "Aasad Khan" },
-    { id: 3, image: "/images/fawad3.jpg", name: "Kamran Khan" },
-    { id: 4, image: "/images/fawad4.jpg", name: "Javed Khan" },
-    { id: 5, image: "/images/imran1.jpg", name: "Fazal Khan" },
-    { id: 6, image: "/images/imran2.jpg", name: "Rahim Khan" },
-    { id: 6, image: "/images/imran3.jpg", name: "Imran Khan" },
-    { id: 7, image: "/images/profile.jfif", name: "Saeed Khan" },
-    { id: 8, image: "/images/fawad1.jpg", name: "Bilal Khan" },
-    { id: 9, image: "/images/fawad2.jpg", name: "Umer Khan" },
-    { id: 10, image: "/images/fawad3.jpg", name: "Usman Khan" },
+    { id: 1, image: "/images/fawad1.jpg", name: "Beth" },
+    { id: 2, image: "/images/fawad2.jpg", name: "Jerry" },
+    { id: 3, image: "/images/fawad3.jpg", name: "Meesicks" },
+    { id: 4, image: "/images/fawad4.jpg", name: "Birdman" },
+    { id: 5, image: "/images/imran1.jpg", name: "Morty" },
+    { id: 6, image: "/images/imran2.jpg", name: "Unity" },
+    { id: 6, image: "/images/imran3.jpg", name: "Random guy" },
+    { id: 7, image: "/images/profile.jfif", name: "Aliens" },
+    { id: 8, image: "/images/fawad1.jpg", name: "Vindicators" },
+    { id: 9, image: "/images/fawad2.jpg", name: "King" },
+    { id: 10, image: "/images/fawad3.jpg", name: "Mars" },
   ]);
   const openChat = (user) => {
     props.openChat(user);
